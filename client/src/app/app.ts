3748c28b35fa50e36f084558d97827c7285ee5ff
youling4438 <youling0106@163.com>
2025-12-25 23:13:32 +0800

    修改组件的默认内容

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,9 +1,7 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
