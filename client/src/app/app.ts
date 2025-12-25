a3f6e5a178448cdf6d2d78129d1c1a8451a28007
youling4438 <youling0106@163.com>
2025-12-25 23:49:35 +0800

    发起http请求

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,10 +1,26 @@
-import { Component, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, signal, OnInit, } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('client');
+export class App implements OnInit {
+  protected readonly title = signal('Dating App');
+  private http = inject(HttpClient);
+
+  ngOnInit() : void{
+	this.http.get('https://localhost:5001/api/members').subscribe({
+		next: response => {
+			console.log(response);
+		},
+		error: error => {
+			console.log(error);
+		},
+		complete: () => {
+			console.log('Request completed');
+		},
+	});
+  }
 }
