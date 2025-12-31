97c6de1c6b53f6f919446dff469eb20f5cad3a08
youling4438 <youling0106@163.com>
2025-12-31 23:05:33 +0800

    End of Section 3

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,26 +1,27 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, inject, signal, OnInit, } from '@angular/core';
+import { Component, inject, signal, OnInit } from '@angular/core';
+import { lastValueFrom, } from 'rxjs';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+	selector: 'app-root',
+	templateUrl: './app.html',
+	styleUrl: './app.css',
 })
 export class App implements OnInit {
-  protected readonly title = signal('Dating App');
-  private http = inject(HttpClient);
+	protected readonly title = signal('Dating App');
+	private http = inject(HttpClient);
+	protected members = signal<any>([]);
 
-  ngOnInit() : void{
-	this.http.get('https://localhost:5001/api/members').subscribe({
-		next: response => {
-			console.log(response);
-		},
-		error: error => {
-			console.log(error);
-		},
-		complete: () => {
-			console.log('Request completed');
-		},
-	});
-  }
+	async ngOnInit(): Promise<void> {
+		this.members.set((await this.getMembers()) as any);
+	}
+
+	async getMembers(): Promise<any> {
+		try {
+			return lastValueFrom(this.http.get('https://localhost:5001/api/members'));
+		} catch (error) {
+			console.error(error);
+			throw new Error('Get HTTP members failed');
+		}
+	}
 }
