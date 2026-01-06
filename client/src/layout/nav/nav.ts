257ae988358dea983331f3951a5285533ecbfe37
youling4438 <youling0106@163.com>
2026-01-07 00:28:36 +0800

    在模板中使用条件语句

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/services/account-service';
 
@@ -11,14 +11,22 @@ import { AccountService } from '../../core/services/account-service';
 export class Nav {
 	private accountService = inject(AccountService);
 	protected creds: any = {};
+	protected loggedIn = signal(false);
+
 	login() {
 		this.accountService.login(this.creds).subscribe({
 			next: response => {
 				console.log(response);
+				this.loggedIn.set(true);
+				this.creds = {};
 			},
 			error: error => {
 				console.error(error);
 			}
 		});
+	}
+
+	logout() {
+		this.loggedIn.set(false);
 	}
 }
