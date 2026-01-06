971c1d0fc85d276a9ee89551021ad4e091d7482d
youling4438 <youling0106@163.com>
2026-01-06 23:57:48 +0800

    添加angular服务

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-nav',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './nav.html',
   styleUrl: './nav.css',
 })
 export class Nav {
-
+	protected creds: any = {};
+	login() {
+		console.log(this.creds);
+	}
 }
