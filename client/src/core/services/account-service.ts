22f944d091bac367d16d02e4bfc5896b77cedbdb
youling4438 <youling0106@163.com>
2026-01-16 23:31:31 +0800

    设置成员卡片样式

--- client/src/core/services/account-service.ts
+++ client/src/core/services/account-service.ts
@@ -2,13 +2,14 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
 import { LoginCreds, RegisterCreds, User } from '../../types/user';
 import { tap } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
 	providedIn: 'root',
 })
 export class AccountService {
 	private http = inject(HttpClient);
-	baseUrl = 'https://localhost:5001/api/';
+	private baseUrl = environment.apiUrl;
 	currentUser = signal<User | null>(null);
 
 	register(creds: RegisterCreds) {
---
+++ client/src/features/members/member-card/member-card.ts
@@ -0,0 +1,13 @@
+import { Component, input } from '@angular/core';
+import { Member } from '../../../types/member';
+import { RouterLink } from "@angular/router";
+
+@Component({
+  selector: 'app-member-card',
+  imports: [RouterLink],
+  templateUrl: './member-card.html',
+  styleUrl: './member-card.css',
+})
+export class MemberCard {
+	member = input.required<Member>();
+}
