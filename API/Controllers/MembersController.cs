148e465a427d7c30cfea69e88f00c66e9fbfd9ba
youling4438 <youling0106@163.com>
2026-01-18 13:27:02 +0800

    添加修改用户信息的API

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -1,3 +1,5 @@
+using System.Security.Claims;
+using API.DTOs;
 using API.Entities;
 using API.Interfaces;
 using Microsoft.AspNetCore.Authorization;
@@ -5,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-     [Authorize]
+    [Authorize]
     public class MembersController(IMemberRepository memberRepository) : BaseApiController
     {
         [HttpGet] //localhost:5001/api/members
@@ -13,19 +15,47 @@ namespace API.Controllers
         {
             return Ok(await memberRepository.GetMembersAsync());
         }
-       
+
         [HttpGet("{id}")] //localhost:5001/api/members/bob-id
         public async Task<ActionResult<Member>> GetMember(string id)
         {
             var member = await memberRepository.GetMemberByIdAsync(id);
-            if(member == null) return NotFound();
+            if (member == null) return NotFound();
             return member;
         }
-        
+
         [HttpGet("{id}/photos")] //localhost:5001/api/members/bob-id/photos
         public async Task<ActionResult<IReadOnlyList<Photo>>> GetMemberPhotos(string id)
         {
             return Ok(await memberRepository.GetPhotosForMemberAsync(id));
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateMember(MemberUpdateDto memberUpdateDto)
+        {
+            var memberId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if(memberId == null)
+            {
+                return BadRequest("Oops - no id found in token");
+            }
+
+            var member = await memberRepository.GetMemberByIdAsync(memberId);
+            if(member == null)
+            {
+                return BadRequest("Could not fount member");
+            }
+
+            member.DisplayName = memberUpdateDto.DisplayName ?? member.DisplayName;
+            member.Description = memberUpdateDto.Description ?? member.Description;
+            member.City = memberUpdateDto.City ?? member.City;
+            member.Country = memberUpdateDto.Country ?? member.Country;
+
+            // memberRepository.Update(member); //Optional
+            if(await memberRepository.SaveAllAsync())
+            {
+                return NoContent();
+            } 
+            return BadRequest("Failed to update user or member");
         }
     }
 }
---
+++ API/DTOs/MemberUpdateDto.cs
@@ -0,0 +1,10 @@
+namespace API.DTOs;
+
+public class MemberUpdateDto
+{
+    public string? DisplayName { get; set; }
+    public string? Description { get; set; }
+    public string? City { get; set; }
+    public string? Country { get; set; }
+
+}
