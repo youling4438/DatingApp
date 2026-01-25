c5ebb90e9eaa43a3a556eb73ef1e42de6795f0f9
youling4438 <youling0106@163.com>
2026-01-26 00:29:20 +0800

    使用FormBuilder构建表单

--- client/src/features/account/register/register.ts
+++ client/src/features/account/register/register.ts
@@ -1,52 +1,40 @@
-import { Component, inject, OnInit, output } from '@angular/core';
+import { Component, inject, output } from '@angular/core';
 import {
 	AbstractControl,
-	FormControl,
+	FormBuilder,
 	FormGroup,
 	ReactiveFormsModule,
 	ValidationErrors,
 	ValidatorFn,
 	Validators,
 } from '@angular/forms';
+import { JsonPipe } from '@angular/common';
 import { RegisterCreds, User } from '../../../types/user';
 import { AccountService } from '../../../core/services/account-service';
-import { JsonPipe } from '@angular/common';
+import { TextInput } from '../../../shared/text-input/text-input';
 
 @Component({
 	selector: 'app-register',
-	imports: [ReactiveFormsModule, JsonPipe],
+	imports: [ReactiveFormsModule, JsonPipe, TextInput],
 	templateUrl: './register.html',
 	styleUrl: './register.css',
 })
-export class Register implements OnInit {
+export class Register {
 	private accountService = inject(AccountService);
+	private fb = inject(FormBuilder);
 	cancelRegister = output<boolean>();
 	protected creds = {} as RegisterCreds;
-	protected registerForm: FormGroup = new FormGroup({});
+	protected registerForm: FormGroup;
 
-	ngOnInit(): void {
-		this.initializeForm();
+	constructor() {
+		this.registerForm = this.fb.group({
+			email: ['', [Validators.required, Validators.email]],
+			displayName: ['', Validators.required],
+			password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
+			confirmPassword: ['', [Validators.required, this.matchValues('password')]],
+		});
 		this.registerForm.controls['password'].valueChanges.subscribe(() => {
 			this.registerForm.controls['confirmPassword'].updateValueAndValidity();
-		});
-	}
-
-	initializeForm() {
-		this.registerForm = new FormGroup({
-			email: new FormControl('thomas.zhang@test.com', [
-				Validators.required,
-				Validators.email,
-			]),
-			displayName: new FormControl('', Validators.required),
-			password: new FormControl('', [
-				Validators.required,
-				Validators.minLength(4),
-				Validators.maxLength(8),
-			]),
-			confirmPassword: new FormControl('', [
-				Validators.required,
-				this.matchValues('password'),
-			]),
 		});
 	}
 
